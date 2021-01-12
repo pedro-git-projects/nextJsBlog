afdb96809fcf9260a8cd48fae22702826a1b6b3b
Pedro Martins <pedro.coding.contact@gmail.com>
2021-01-12 12:24:16 -0300

    refactored index components

--- next-js-blog/pages/index.js
+++ next-js-blog/pages/index.js
@@ -1,81 +1,20 @@
-import { Row, Col, Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import PageLayout from "components/PageLayout";
-import AuthorIntro from 'components/AuthorIntro'
+import AuthorIntro from "components/AuthorIntro";
+import CardItem from "components/CardItem";
+import CardListItem from "components/CardListItem";
 
 export default function Home() {
   return (
     <PageLayout>
       <AuthorIntro />
       <hr />
-      {/* className from props */}
-
       <Row className="mb-5">
         <Col md="10">
-          {/* CardListItem STARTS */}
-          <Card className={`fj-card fj-card-list`}>
-            <div className="card-body-wrapper">
-              <Card.Header className="d-flex flex-row">
-                <img
-                  src={"https://via.placeholder.com/150"}
-                  className="rounded-circle mr-3"
-                  height="50px"
-                  width="50px"
-                  alt="avatar"
-                />
-                <div>
-                  <Card.Title className="font-weight-bold mb-1">
-                    Placeholder Author
-                  </Card.Title>
-                  <Card.Text className="card-date">Placeholder Date</Card.Text>
-                </div>
-              </Card.Header>
-              <Card.Body>
-                <Card.Title className="card-main-title">
-                  Placeholder Title
-                </Card.Title>
-                <Card.Text>Placehodler Subtitle</Card.Text>
-              </Card.Body>
-            </div>
-            <a href="#" className="card-button">
-              Read More
-            </a>
-          </Card>
-          {/* CardListItem ENDS */}
+          <CardListItem />
         </Col>
-
         <Col md="4">
-          <Card className={`fj-card`}>
-            <div className="card-body-wrapper">
-              <Card.Header className="d-flex flex-row">
-                <img
-                  src={"https://via.placeholder.com/150"}
-                  className="rounded-circle mr-3"
-                  height="50px"
-                  width="50px"
-                  alt="avatar"
-                />
-                <div>
-                  <Card.Title className="font-weight-bold mb-1">
-                    Placeholder Author
-                  </Card.Title>
-                  <Card.Text className="card-date">Placeholder Date</Card.Text>
-                </div>
-              </Card.Header>
-              <div className="view overlay">
-                <Card.Img
-                  src="https://via.placeholder.com/250"
-                  alt="Card image cap"
-                />
-              </div>
-              <Card.Body>
-                <Card.Title className="card-main-title">
-                  Placeholder Title
-                </Card.Title>
-                <Card.Text>Placehodler Subtitle</Card.Text>
-              </Card.Body>
-            </div>
-            <a className="card-button">Read More</a>
-          </Card>
+          <CardItem />
         </Col>
       </Row>
     </PageLayout>
