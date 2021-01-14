ebf952e946517236d6fdbbd21ca22b025bd78688
Pedro Martins <pedro.coding.contact@gmail.com>
2021-01-14 17:09:17 -0300

    code styling

--- next-js-blog/pages/_app.js
+++ next-js-blog/pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '/run/media/pedrocoding/1a3a036c-b02d-40e8-b092-c18ce17020b5/Dev/blog/next-js-blog/node_modules/highlight.js/styles/dracula.css'
 import 'styles/index.scss'
 
 function MyApp({ Component, pageProps }) {
